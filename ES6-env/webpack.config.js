f2f16fcaf83290ded821c54296b08e98d6048d34
PLDaily <pcd12321@gmail.com>
2017-01-24 20:33:43 +0800

    支持generate、yield

--- ES6-env/webpack.config.js
+++ ES6-env/webpack.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  entry: './main',//入口文件
+  entry: ['babel-polyfill', './main.js'],//入口文件
   output: {
     filename: 'bundle.js'//输出文件
   },
   module: {
     loaders: [{
-      test: /\.js$/,//正则匹配文件，对其进行解析
+      test: /\.js$/,//正则匹配文件，对其进e行解析
       exclude: /node_modules/,//不对node_modules里的js文件进行解析
       loader: 'babel',//使用babel加载器
       query: {
-        presets: ['es2015']//解析成ES5的形式
+        presets: ['es2015', 'stage-0']//解析成ES5的形式
       }
     }]
   }
